28b2d1e063180e6a7d9e07357152370b6f1aee81
Clutter Boutique <the@clutter.boutique>
2022-04-24 02:52:50 -0400

    vim

---
+++ functions/fromTweet.js
@@ -0,0 +1,48 @@
+const TARGET_URL = 'https://api.twitter.com/2/tweets/'
+const START_REGEX = RegExp('^.[^🟩|⬛|🟨]*')
+const END_REGEX = RegExp('[^🟩|⬛|🟨]*$')
+
+export async function onRequest(ctx) {
+  try {
+    let tweetId = getTweetId(ctx)
+    let response =  await getTweetJson(tweetId, ctx.env.BEARER_TOKEN)
+    if (Object.keys(response).includes('data')){
+      let wordle = extractWordle(response)
+      let hash = wordleToBeatHash(wordle)
+      return new Response(wordle + "\n" + hash)
+    } else {
+      return new Response("No tweet with wordle found for id ", tweetId)
+    }
+  } catch (err){
+    return new Response(err.message)
+  }
+}
+
+
+async function getTweetJson(tweetId, bearerToken){
+  let target = TARGET_URL + tweetId
+  let headers =  {headers: {Authorization: `Bearer ${bearerToken}`}}
+  let response = await fetch(target, headers)
+  return await response.json()
+}
+
+function wordleToBeatHash(wordle){
+  return wordle.replace("\n", "")
+    .replace('🟨', '1')
+    .replace('🟩', '1')
+    .replace('⬛', '0')
+}
+
+function getTweetId(ctx){
+  let url = new URL(ctx.request.url)
+  let searchParams = new URLSearchParams(url.search)
+  return searchParams.get('tweet')
+}
+
+function extractWordle(response){
+  let text = response['data']['text']
+  return text.replace(START_REGEX, '').replace(END_REGEX, '')
+}
+
+
+
